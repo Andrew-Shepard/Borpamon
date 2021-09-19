bcde2e6d1e5a1448f86c3be914a6c8cc816904d6
andre <andrewshepard78@gmail.com>
2021-09-18 22:56:40 -0400

    Started adding move selector menu

--- Assets/Scripts/Battle/BattleDialogBox.cs
+++ Assets/Scripts/Battle/BattleDialogBox.cs
@@ -5,9 +5,19 @@ using UnityEngine.UI;
 
 public class BattleDialogBox : MonoBehaviour
 {
-    [SerializeField] Text dialogText;
     [SerializeField] int lettersPerSecond;
+    [SerializeField] Color highlightedColor;
 
+    [SerializeField] Text dialogText;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveDetails;
+
+    [SerializeField] List<Text> actionTexts;
+    [SerializeField] List<Text> moveTexts;
+
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
     public void SetDialog(string dialog)
     {
         dialogText.text = dialog;
@@ -20,6 +30,43 @@ public class BattleDialogBox : MonoBehaviour
         {
             dialogText.text += letter;
             yield return new WaitForSeconds(1f/lettersPerSecond);
+        }
+    }
+
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+    }
+
+    public void UpdateActionSelection(int selectedAction)
+    {
+        for (int i=0; i<actionTexts.Count; i++)
+        {
+            if (i == selectedAction)
+            {
+                actionTexts[i].color = highlightedColor;
+            }
+            else
+            {
+                actionTexts[i].color = Color.black;
+            }
+        }
+    }
+    public void SetMoveNames(List<Move> moves)
+    {
+        for (int i = 0 ; i < moveTexts.Count; i++){
+            if (i < moves.Count)
+                moveTexts[i].text = moves[i].Base.Name;
+            else
+                moveTexts[i].text = "-";
         }
     }
 }
--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState { Start, PlayerAction, PlayerMove, EnemyMove, Busy }
 public class BattleSystem : MonoBehaviour
 {
     [SerializeField] BattleUnit playerUnit;
@@ -12,18 +13,82 @@ public class BattleSystem : MonoBehaviour
 
     [SerializeField] BattleDialogBox dialogBox;
 
+    BattleState state;
+    int currentAction;
+
     private void Start()
     {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    public void SetupBattle()
+    public IEnumerator SetupBattle()
     {
         playerUnit.Setup();
         enemyUnit.Setup();
         playerHud.SetData(playerUnit.Borpamon);
         enemyHud.SetData(enemyUnit.Borpamon);
 
-        StartCoroutine( dialogBox.TypeDialog($"A wild {12} appeared!") );
+        dialogBox.SetMoveNames(playerUnit.Borpamon.Moves);
+        yield return 
+            StartCoroutine( dialogBox.TypeDialog($"A wild {enemyUnit.Borpamon.Borpamon_base.Name} appeared!") );
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
     }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action"));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    void PlayerMove()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action"));
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableMoveSelector(true);
+    }
+
+    void HandleActionSelection()
+    {   //1 is run, 0 is fight
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentAction < 1)
+                currentAction++;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentAction > 0)
+                currentAction--;
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentAction == 0)
+            {
+                //Fight
+                PlayerMove();
+
+            }
+            else if (currentAction == 1)
+            {
+                //Run
+
+            }
+        }
+    }
+    private void Update()
+    {
+        if(state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+    }
+    
+
 }
